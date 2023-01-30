9ffcdb08eca8a58c4580371c4e4dee0802fb0f32
fgonzalezformacion <70232739+fgonzalezformacion@users.noreply.github.com>
2023-01-30 13:25:00 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 
-while True:
-    try:
-        num1 = int(st.input("Ingresa el primer número: "))
-        num2 = int(st.input("Ingresa el segundo número: "))
-        resultado = num1 + num2
-        st.print("La suma es:", resultado)
-        break
-    except ValueError:
-        st.print("ese número no es válido, por favor ingresa un entero")
+import streamlit as st
+
+st.title("Sumador de números")
+
+num1 = st.number_input("Introduce el primer número:")
+num2 = st.number_input("Introduce el segundo número:")
+
+result = num1 + num2
+
+st.write("El resultado de la suma es:", result)
