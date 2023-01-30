dea08dbe8db163d1bac7d5a4a3cfc5e746361de4
fgonzalezformacion <70232739+fgonzalezformacion@users.noreply.github.com>
2023-01-30 13:15:03 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 
+import streamlit as st
 while True:
     try:
-        num1 = int(input("Ingresa el primer número: "))
-        num2 = int(input("Ingresa el segundo número: "))
+        num1 = st.int(input("Ingresa el primer número: "))
+        num2 = st.int(input("Ingresa el segundo número: "))
         resultado = num1 + num2
         print("La suma es:", resultado)
         break
