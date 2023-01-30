c6a79072f154e873560c77adfa6d170e2895f015
fgonzalezformacion <70232739+fgonzalezformacion@users.noreply.github.com>
2023-01-30 13:20:22 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
 
-import streamlit as st
 while True:
     try:
         num1 = int(st.input("Ingresa el primer número: "))
         num2 = int(st.input("Ingresa el segundo número: "))
         resultado = num1 + num2
-        print("La suma es:", resultado)
+        st.print("La suma es:", resultado)
         break
     except ValueError:
-        print("ese número no es válido, por favor ingresa un entero")
+        st.print("ese número no es válido, por favor ingresa un entero")
